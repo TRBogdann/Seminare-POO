980431fc4fa495aa742943f3fa667912a0efdfa4
Marmota_in_coma <40302782+Mahou-sama666@users.noreply.github.com>
2023-11-02 17:15:54 +0200

    Trebuie combinate cu ce ai tu
    tre sa disc sa vedem ce ai tu si ce am eu si cum le putem imbina

--- Proiect/calculator.h
+++ Proiect/calculator.h
@@ -1,20 +1,27 @@
+#pragma once
 #include "checker.h"
-#include "math_functions.h"
 #include <cstring>
+#include <iostream>
 
-class Calculator
-{
+class Calculator{
 public:
+//Constructor/ Destructor
 Calculator();
+Calculator(char* expression);
 ~Calculator();
-double evalExpr();
+//Getter
+char* getExpression();
+Checker getChecker();
+//Setter
 void setExpr(char *expr);
-char* getExpr();
+void setChecker(Checker checker);
+//Overload
+friend std::ostream& operator<<(std::ostream& os, const Calculator& c);
+friend std::istream& operator>>(std::istream& is, Calculator& c);
+//Metode diverse
+void evalExpr();
 
 private:
 Checker checker;
 char *expression;
-double evalSeg(char *str,int len,char flag);
-double string_to_double(char *str,int len);
-
 };
---
+++ Proiect/equation.h
@@ -0,0 +1,46 @@
+#include "math_functions.h"
+#include "operators.h"
+#include "checker.h"
+#pragma once
+#include "calculator.h"
+//#include <iostream>
+#include <cstring>
+struct solutii{
+    double x1;
+    double x2;
+};
+class Equation{
+private:
+    char Unk;
+    double a, b, c, d;
+    int  degree;
+public:
+//Constructori
+    Equation(){}
+    Equation(int degree, char Unk, double b, double c, double d):a(0),b(b),c(c), d(d), degree(degree){}
+    Equation(int degree, char Unk, double a, double b, double c, double d) : Unk(Unk), a(a), b(b), c(c), d(d) {}
+    Equation(const Equation& e);
+//Getteri
+    char getUnk(){ return Unk;}
+    double getA(){ return a;}
+    double getB(){ return b;}
+    double getC(){ return c;}
+    double getD(){ return d;}
+    int getdegree(){ return degree;}
+//Setteri
+    void setUnk(char Unk);
+    void setA(double a);
+    void setB(double b);
+    void setC(double c);
+    void setD(double d);
+    void setDegree(int degree);
+//Overloads
+    friend std::ostream& operator<<(std::ostream& os,const Equation& e);
+    friend std::istream& operator>>(std::istream& is, Equation& e);
+    friend Equation& operator!(const Equation& e);
+    friend Equation& operator^(const Equation &e, float y);
+//Alte Metode
+    double f_degree();
+    solutii s_degree();
+    //double n_degree(const Equation& e);
+};
